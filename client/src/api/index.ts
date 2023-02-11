9739313e535b5de3d70892f1f7c2c3916ae51bac
Yogesh D <yogdat@gmail.com>
2023-02-11 17:04:53 +0530

    updated baseURL for APIs

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,36 +1,36 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "https://memories-node-react.herokuapp.com",
+  baseURL: 'https://social-mern.up.railway.app/',
   // baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req: any) => {
-  if (localStorage.getItem("profile")) {
+  if (localStorage.getItem('profile')) {
     req.headers.authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("profile") || "{}").token
+      JSON.parse(localStorage.getItem('profile') || '{}').token
     }`;
   }
   return req;
 });
 
-export const fetchPosts = (page:any) => API.get(`/posts?page=${page}`);
+export const fetchPosts = (page: any) => API.get(`/posts?page=${page}`);
 export const fetchPost = (id: any) => API.get(`/posts/${id}`);
 export const fetchPostsBySearch = (searchQuery: any) =>
   API.get(
-    `/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${
+    `/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${
       searchQuery.tags
     }`
   );
-export const createPost = (newPost: any) => API.post("/posts", newPost);
+export const createPost = (newPost: any) => API.post('/posts', newPost);
 export const uploadPostImage = (image: any) =>
-  API.post("/image/uploadImage", image);
+  API.post('/image/uploadImage', image);
 export const deletePostImage = (fileId: string) =>
-  API.post("/image/deleteImage", { fileId });
+  API.post('/image/deleteImage', { fileId });
 export const updatePost = (id: number, updatedPost: any) =>
   API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id: number) => API.delete(`/posts/${id}`);
 export const likePost = (id: number) => API.patch(`/posts/${id}/likePost`);
 
-export const signIn = (formData: any) => API.post("/user/signIn", formData);
-export const signUp = (formData: any) => API.post("/user/signUp", formData);
+export const signIn = (formData: any) => API.post('/user/signIn', formData);
+export const signUp = (formData: any) => API.post('/user/signUp', formData);
